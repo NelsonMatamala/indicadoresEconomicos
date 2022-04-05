6185fa1af162edaa7f7dce4c310001ae0a26c858
Nelson <nel.matamala@gmail.com>
2022-04-05 11:12:01 -0400

    Se quito unidad_medida en DataIndicador

--- app/src/main/java/cl/nelsonmc/indicadores/model/DataIndicador.java
+++ app/src/main/java/cl/nelsonmc/indicadores/model/DataIndicador.java
@@ -4,17 +4,12 @@ import java.util.ArrayList;
 
 public class DataIndicador {
     private String nombre;
-    private String unidad_medida;
     private ArrayList<SerieIndicador> serie;
 
     public String getNombre() {
         return nombre;
     }
-
-    public String getUnidad_medida() {
-        return unidad_medida;
-    }
-
+    
     public ArrayList<SerieIndicador> getSerie() {
         return serie;
     }
