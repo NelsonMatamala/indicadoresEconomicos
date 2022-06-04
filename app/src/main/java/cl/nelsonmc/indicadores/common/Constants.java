8e773ca8bd9e6d81d353db9296d5bd4a8489e3a4
Nelson <nel.matamala@gmail.com>
2022-06-04 17:51:17 -0400

    Constantes indicadores

--- app/src/main/java/cl/nelsonmc/indicadores/common/Constants.java
+++ app/src/main/java/cl/nelsonmc/indicadores/common/Constants.java
@@ -1,7 +1,24 @@
 package cl.nelsonmc.indicadores.common;
 
 public class Constants {
-    public static final String LIGHTMODE = "Light Mode";
-    public static final String DARKMODE = "Dark Mode";
-    public static final String PREDETERMINADO = "Predeterminado";
+    //MODE UI
+    public static final String LIGHTMODE = "lightMode";
+    public static final String DARKMODE = "darkMode";
+    public static final String PREDETERMINADO = "predeterminado";
+
+    //SharedPreferences
+    public static final String MODE_UI = "optionMode";
+    public static final String DATA_INDICADORES = "dataIndicadores";
+
+    //Indicadores
+    public static final String DOLAR = "dolar";
+    public static final String EURO = "euro";
+    public static final String UF = "uf";
+    public static final String IVP = "ivp";
+    public static final String IPC = "ipc";
+    public static final String UTM = "utm";
+    public static final String IMACEC = "imacec";
+    public static final String COBRE = "cobre";
+    public static final String DESEMPLEO = "desempleo";
+    public static final String BITCOIN = "bitcoin";
 }
