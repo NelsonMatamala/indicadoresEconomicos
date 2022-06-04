be01ea85f1a4316f37ff65704fd3e9f856b32370
Nelson <nel.matamala@gmail.com>
2022-06-04 12:58:08 -0400

    add sharedpreferences

--- app/src/main/java/cl/nelsonmc/indicadores/BaseApplication.java
+++ app/src/main/java/cl/nelsonmc/indicadores/BaseApplication.java
@@ -1,26 +1,29 @@
 package cl.nelsonmc.indicadores;
 
+import static cl.nelsonmc.indicadores.common.Constants.MODE_UI;
+
 import android.app.Application;
-import android.preference.PreferenceManager;
 
 import androidx.appcompat.app.AppCompatDelegate;
 
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
 import cl.nelsonmc.indicadores.common.Constants;
-import cl.nelsonmc.indicadores.di.DaggerRetrofitComponent;
-import cl.nelsonmc.indicadores.di.RetrofitComponent;
-import cl.nelsonmc.indicadores.di.RetrofitModule;
-
+import cl.nelsonmc.indicadores.common.SharedPreferencesManager;
+import cl.nelsonmc.indicadores.repository.retrofitdi.DaggerRetrofitComponent;
+import cl.nelsonmc.indicadores.repository.retrofitdi.RetrofitComponent;
+import cl.nelsonmc.indicadores.repository.retrofitdi.RetrofitModule;
 
 public class BaseApplication extends Application {
     private RetrofitComponent retrofitComponent;
+    public static SharedPreferencesManager sharedPreferences;
 
     @Override
     public void onCreate() {
         super.onCreate();
         AndroidThreeTen.init(this);
         retrofitComponent = DaggerRetrofitComponent.builder().retrofitModule(new RetrofitModule()).build();
+        sharedPreferences = SharedPreferencesManager.getInstance(this);
         uiModeVerification();
     }
 
@@ -29,7 +32,7 @@ public class BaseApplication extends Application {
     }
 
     private void uiModeVerification() {
-        String modeOptionString = PreferenceManager.getDefaultSharedPreferences(this).getString("option_mode", Constants.PREDETERMINADO);
+        String modeOptionString = sharedPreferences.getDataByName(MODE_UI);
         if (modeOptionString.equals(Constants.DARKMODE)) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         } else if (modeOptionString.equals(Constants.LIGHTMODE)) {
