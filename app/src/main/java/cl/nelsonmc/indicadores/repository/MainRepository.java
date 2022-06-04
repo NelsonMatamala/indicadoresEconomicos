df279648c8d01d68321184a892f0f0a912e61398
Nelson <nel.matamala@gmail.com>
2022-06-04 18:06:36 -0400

    getBitcoinData()

--- app/src/main/java/cl/nelsonmc/indicadores/repository/MainRepository.java
+++ app/src/main/java/cl/nelsonmc/indicadores/repository/MainRepository.java
@@ -58,6 +58,7 @@ public class MainRepository {
         getIMACECData();
         getCobreData();
         getDesempleoData();
+        getBitcoinData();
     }
 
     public void clearDisposable() {
